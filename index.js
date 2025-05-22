27db4d69fd4758cf2059aa415b96ddf9eb220115
fuzzbear <58638135+fuzzbear0001@users.noreply.github.com>
2025-05-22 13:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds, 
-		GatewayIntentBits.GuildMessages, 
-		GatewayIntentBits.GuildPresences, 
-		GatewayIntentBits.GuildMessageReactions, 
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent
-	], 
-	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+
+    GatewayIntentBits.GuildMembers,  // <-- ADD THIS
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
 });
 
 const fs = require('fs');
 const config = require('./config.json');
-require('dotenv').config() // remove this line if you are using replit
+require('dotenv').config(); // remove if using Replit
 
-client.commands = new Collection()
-client.aliases = new Collection()
+client.commands = new Collection();
+client.aliases = new Collection();
 client.slashCommands = new Collection();
 client.buttons = new Collection();
 client.prefix = config.prefix;
 
 module.exports = client;
 
-
 fs.readdirSync('./handlers').forEach((handler) => {
-  require(`./handlers/${handler}`)(client)
+  require(`./handlers/${handler}`)(client);
 });
 
-
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
